442f0dd4c10a54a365b20c1330908875b3066114
cecilesauder <cecile.sauder@gmail.com>
2017-12-27 14:50:05 +0100

    ajout de la différence de votes sur le graphe

--- server.R
+++ server.R
@@ -23,6 +23,8 @@ shinyServer(function(input, output) {
       str_extract( "[[:digit:]]+" ) %>%
       as.numeric()
     
+    votes_lexie <- votes[id == id_lexie]
+
     nb_enfants <- length(votes)
     
     df <- 
@@ -31,7 +33,9 @@ shinyServer(function(input, output) {
              nameKid = 1:nb_enfants,
              col = rep("black", nb_enfants)) %>%
       rownames_to_column("classement") %>%
-      mutate(classement = as.numeric(classement))
+      mutate(classement = as.numeric(classement),
+             diff_vote = votes - votes_lexie) %>%
+      mutate(diff_vote = if_else(diff_vote > 0, paste0("+",diff_vote), as.character(diff_vote)))
     
     
     df$nameKid[id == id_lexie] <- "Lexie"
@@ -39,7 +43,7 @@ shinyServer(function(input, output) {
     df$col <- as.factor(df$col)
     
     invalidateLater(8000)
-    
+
     df
   })
   
@@ -70,25 +74,29 @@ shinyServer(function(input, output) {
     
     myPalette <- c('#999999','#FF00FF')
     names(myPalette) <- levels(df$col)
+    
     df_text <- data.frame ( 
       numKid = df %>% filter(nameKid == "Lexie") %>% pull(numKid),
       votes = df %>% filter(nameKid == "Lexie") %>% pull(votes) %>% add(100),
       col = "black" )
     
+    
     gg <- df %>% 
       filter(classement < 21) %>%
-      ggplot(aes(x = numKid, y = votes , fill = col)) + 
+      ggplot(aes(x = numKid, y = votes , fill = col, label = diff_vote)) + 
       theme_light() +
 #      theme(axis.text.y = element_text(face="bold", size = 12, color = myPalette[df$col])) +
       scale_fill_manual(values=c('#999999','#FF00FF')) +
-      xlab("bébés") +
+      xlab("classement des 20 premiers") +
       ggtitle("Les 10 premiers du concours sont pré-selectionnés") +
       geom_bar_interactive(aes(tooltip = votes, data_id = nameKid),stat="identity") +
       guides(fill = FALSE) +
       coord_flip() + 
       scale_x_reverse() +
       geom_vline(xintercept = 10.5, linetype = 2) +
-      geom_text(data = df_text, mapping = aes(label = "Lexie"))
+      geom_text(mapping = aes(y=-30, hjust = 1)) +
+      geom_text(data = df_text, mapping = aes(label = "Lexie")) +
+      ylim(-120, max(df$votes))
       
 
     ggiraph(code = print(gg), hover_css = "fill-opacity:.6;cursor:pointer;",
